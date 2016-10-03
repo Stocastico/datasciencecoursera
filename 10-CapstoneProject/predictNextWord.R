2c6f492d0838e6ed7ffc722339ee05adb231d93f
stocastico <stocastico@gmail.com>
2016-10-03 14:46:32 +0200

    Keep work on word prediction

--- 10-CapstoneProject/predictNextWord.R
+++ 10-CapstoneProject/predictNextWord.R
@@ -1,16 +1,35 @@
 #Assumes input has already been processed.
 #words is a char vector of length 3
 #returns the top 5 predictions of the model
+library(slam)
+library(matrix)
 
 predictNextWord <- function(words, dict, matrix4Gram, matrix3Gram, matrix2Gram, trieIdx) {
   
-  #ok, using the trie map from words to index in the dictionary
-  if (words[1] %in% myDict) {
+  idxUnk <- longest_match(trie = 'trieIdx', to_match = '<UNK>')
+  idx <- c(idxUnk, idxUnk, idxUnk)
+  
+  for (k in 1:3) {
+    #ok, using the trie map from words to index in the dictionary
+    if (words[k] %in% myDict) {
+      idx[k] <- longest_match(trie = 'trieIdx', to_match = words[k])
+    }
+  }
+  
+  wordsArray <- drop_simple_sparse_array(matrix4Gram[idx[1], idx[2], idx[3], ])
+  
+  numMatches <- nnzero(wordsArray$v)
+  
+  if ( numMatches >= 5) {
+    topFreq <- tail(sort.int(wordsArray$v, partial = length(wordsArray$v) - 4), 5)
+    topIdx <- wordsArray$i[topFiveFreq]
+  } else if ( numMatches > 0 ) {
+    topFreq <- tail(sort.int(wordsArray$v, partial = length(wordsArray$v) - (numMatches-1)), numMatches)
+    topIdx <- wordsArray$i[topFiveFreq]
+  } else { # todo 
     
   }
-  else {
-    
-  }
+  #order <- order(wordsArray, decreasing = TRUE)
   
   #if necessary
   #predictWithTrigram(words(2:3), dict, matrix3Gram, matrix2Gram, trieIdx)
